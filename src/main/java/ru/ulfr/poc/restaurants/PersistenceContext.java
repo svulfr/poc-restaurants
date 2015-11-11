29477bf7cabf7ebd726bc45e7d3b86b9c1a3ed2e
svulfr <svulfr@gmail.com>
2015-11-11 21:53:36 +0300

    pro configuration changes

--- src/main/java/ru/ulfr/poc/restaurants/PersistenceContext.java
+++ src/main/java/ru/ulfr/poc/restaurants/PersistenceContext.java
@@ -45,7 +45,7 @@ public class PersistenceContext {
 
         Properties jpaProperties = new Properties();
         jpaProperties.put("hibernate.dialect", "org.hibernate.dialect.MySQLInnoDBDialect");
-        jpaProperties.put("hibernate.show_sql", "true");
+        jpaProperties.put("hibernate.show_sql", "false");
         jpaProperties.put("hibernate.format_sql", "true");
 
         entityManagerFactoryBean.setJpaProperties(jpaProperties);
